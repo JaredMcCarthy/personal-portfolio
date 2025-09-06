17538e20ec0b760bf4a47023baab8f03e2b0f172
Jared Alexander Lopez McCarthy <jared.lopezmccarthy05@gmail.com>
2025-09-06 02:18:34 -0600

    backend de carrusel de proyectos trabajada pero no terminada aun, a ver si se termina manana

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
-function expandirTarjeta(tarjeta) {
-  tarjeta.classList.add("tarjeta-expandida");
-}
+const LeWindows = require("nodemailer/lib/mime-node/le-windows");
 
-function encogerTarjeta(tarjeta) {
-  tarjeta.classList.remove("tarjeta-expandida");
-}
+let imagenes = [
+  {
+    url: "Imagenes Proyectos/img1.webp",
+    nombre: "Proyecto 1",
+    descripcion: "Esta es la descripcion del proyecto 1 Portafolio Personal",
+  },
+  {
+    url: "Imagenes Proyectos/img2.webp",
+    nombre: "Proyecto 2",
+    descripcion: "Esta es la descripcion del proyecto 1 Portafolio Personal",
+  },
+  {
+    url: "Imagenes Proyectos/img3.webp",
+    nombre: "Proyecto 3",
+    descripcion: "Esta es la descripcion del proyecto 1 Portafolio Personal",
+  },
+];
+
+let atras = document.getElementById("atras");
+let adelante = document.getElementById("adelante");
+let imagen = document.getElementById("imagen");
+let puntos = documento.getElementById("puntos");
+let texto = document.getElementById("texto");
+let actual = 0;
+
+atras.addEventListener("click", function () {
+  actual -= 1;
+
+  if (actual == -1) {
+    actual = imagenes.length - 1;
+  }
+
+  imagen.innerHTML = `<img class="img" src=" ${imagenes[actual].url}" alt="logo pagina" loading="lazy"></img>`;
+  texto.innerHTM; = `
+  <h3>${imagenes[actual].nombre}</h3>
+  <p>${imagenes[actual].descripcion}</p>`
+});
