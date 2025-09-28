77f76fb7d8cad5ec340ece581be4f0834406427d
Jared Alexander Lopez McCarthy <jared.lopezmccarthy05@gmail.com>
2025-09-28 03:02:36 -0600

    agregandos obteniendo datos y formulario del backend con test en consola por si acaso

--- FormHandler.js
+++ FormHandler.js
@@ -1,6 +1,27 @@
-document
-  .getElementById("formContacto")
-  .addEventListener("submit", function (event) {
-    event.preventDefault(); // este aqui detiene el envio normal de los datos
-    console.log("Formulario se detuvo, El proximo paso es obtener esos datos.");
-  });
+console.log("El archivo se estar cargando"); //mensaje al carga si funciona
+
+const miForm = document.getElementById("formContacto"); //busca el ele con ese id
+console.log("Formulario:", miForm); //ahi muestra si existe o no
+
+if (miForm) {
+  console.log("Si se encontro el formulario");
+  miForm.onsubmit = function (event) {
+    event.preventDefault(); //detiene el form tradicional al reca la pag
+    console.log("Se detuvo el formulario y funciono");
+    //aqui obtenemos los datos de donde estan los inputs
+    const nombre = document.querySelector('input[name="name"').value;
+    const email = document.querySelector('input[name="email"').value;
+    const asunto = document.querySelector('input[name="asunto"').value;
+    const mensaje = document.querySelector('textarea[name="mensaje"]').value;
+
+    // muestra estos datos en la consola para revisar que esten bien
+    // esta parte de aqui es solo de test en console para ver si funciona nada mas
+    console.log("Datos capturados:");
+    console.log("Nombre:", nombre);
+    console.log("Email:", email);
+    console.log("asunto:", asunto);
+    console.log("mensaje:", mensaje);
+  };
+} else {
+  console.log("Error al buscar el formulario");
+}
